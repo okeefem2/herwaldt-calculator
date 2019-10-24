cc146aa6fcdb30089e1a55f3d8ee9357c830a8e6
herwaldt <1995ekaj@gmail.com>
2019-10-24 18:51:25 -0400

    calculation functionality, history, repeat calc and delete history

--- main.js
+++ main.js
@@ -1,107 +1,158 @@
 // use the variables below to run Math function
-let val1;
-let val2;
-let op;
+let val1 = '';
+let val2 = '';
+let op = '';
+let history = ' ';
+let lastAnsw;
+let lastHist;
+let prevAns;
+
 
 function inputNum(number) {
-    const numberString = document.getElementById("calc").value;
-
-    if (isNaN(numberString)) {
-        // Show error
-        return;
-    }
-    const number = Number(numberString);
-    //if value is 0 then it is replaced by selected value. Otherwise adds to display value
-    if (document.getElementById("calc").value == 0 || isNaN(document.getElementById("calc").value) ){
-        if (document.getElementById("calc").value == 0 + ".") {
-            //nested if to account for 0. being treated as NaN
-            document.getElementById("calc").value = document.getElementById("calc").value + number;
+    const currentNumber = document.getElementById('calc').value;
+    // console.log(val1);
+    // If op is undefined then work on creating var1 //
+    if ( lastAnsw !== undefined ) {
+        if ( op !== '' ) {
+            lastAnsw = undefined;
+            document.getElementById('calc').value = number;
+            val2 = document.getElementById('calc').value;
         } else {
-            document.getElementById("calc").value = number;
+        // do nothing so that user does not override answer from calculation before choosing operator //
         }
-    } else { 
-        document.getElementById("calc").value = document.getElementById("calc").value + number;
-    }
-}
-
-function allClearNum(){
-    //clears input to 0 and clears all variables
-    document.getElementById("calc").value = 0;
-    val1 =undefined;
-    val2 =undefined;
-    op = undefined;
-}
-
-function makeNeg(){
-    if(document.getElementById("calc").value==0 || isNaN(document.getElementById("calc").value) ){
-        //allows a leading 0 with decimal to happen
-        document.getElementById("calc").value = 0;
-    }else{
-    document.getElementById("calc").value = "-" + document.getElementById("calc").value;
-    }
-}
-
-
-function clearNum(){
-    document.getElementById("calc").value = 0;
-}
-
-function decimal(){
-    if(document.getElementById("calc").value==0 || isNaN(document.getElementById("calc").value) ){
-        //allows a leading 0 with decimal to happen
-        document.getElementById("calc").value = 0 + ".";
-    }else{
-        if(document.getElementById("calc").value.includes('.')){
-            //stops user from entering multiple decimals
-            document.getElementById("calc").value=document.getElementById("calc").value;
-        } else{
-            //adds decimal to end of value
-            document.getElementById("calc").value = document.getElementById("calc").value + ".";
-        }
-    }
-}
-
-function myOperator(myOp){
-    if(op !== undefined && val1 !== undefined){
-        //skip to calculate if val1 and op are defined
-        calculate();
-    } else{
-        if(op==undefined){
-            //sets val1 to what is currently inputted. sets operator to user selected option
-            val1 = document.getElementById("calc").value;
-            op=myOp;
-            document.getElementById("calc").value = myOp;
+    } else if ( op == '' ) {
+        if ( currentNumber == 0 ) {
+            document.getElementById('calc').value = number;
+            val1 = number;
         } else {
-            //overides previous operation in case user deviates from original selected operator
-            op=myOp;
-            document.getElementById("calc").value = myOp;
+            document.getElementById('calc').value = currentNumber + number;
+            val1 = document.getElementById('calc').value;
         }
-    }
-}
-
-function calculate(){
-    if(val1 == undefined || op == undefined || isNaN(document.getElementById("calc").value)){
-        //do nothing if all variables are not defined or input value is not a number.
-            //!!!Val2 is equal to the input field at time of calculation
     } else {
-        val2 = document.getElementById("calc").value
-        document.getElementById("calc").value = val1 + val2;
-        val1 =undefined;
-        val2 =undefined;
-        op = undefined;
+        if ( currentNumber == 0 ) {
+            document.getElementById('calc').value = number;
+            val2 = number;
+        } else {
+            document.getElementById('calc').value = val2 + number;
+            val2 = document.getElementById('calc').value;
+        }
     }
 }
 
-function doCalculation(number1, number2, operator) {
-    switch (operator) {
-        case '+':
-            return number1 + number2;
-        case '-':
-            return number1 - number2;
-        case '*':
-            return number1 * number2;
-        default:
-            return 0;
+
+function myOperator(myOp) {
+    console.log(history);
+    if ( lastAnsw !== undefined ) {
+        val1 = lastAnsw;
+        lastAnsw = undefined;
+        op = myOp;
+        document.getElementById('op').value = myOp;
+        histroy = history + val1 + op;
+        document.getElementById('hist').innerHTML = history;
+    } else if ( op === '' ) {
+        op = myOp;
+        document.getElementById('op').value = myOp;
+        history = history + val1 + op;
+        document.getElementById('hist').innerHTML = history;
+    } else if ( val2 === '' ) {
+        op = myOp;
+        document.getElementById('op').value = myOp;
+        history = val1 + op;
+        document.getElementById('hist').innerHTML = history;
+    } else {
+        calculate();
+        document.getElementById('op').value = myOp;
+        op = myOp;
+        history = history + op;
+        document.getElementById('hist').innerHTML = history;
     }
 }
 
+function calculate() {
+    history = history + val2;
+    document.getElementById('hist').innerHTML = history;
+    let answer;
+    switch (op) {
+    case '+':
+        answer = Number(val1) + Number(val2);
+        break;
+    case '-':
+        answer = Number(val1) - Number(val2);
+        break;
+    case '*':
+        answer = Number(val1) * Number(val2);
+        break;
+    case '&#247;':
+        answer = Number(val1) / Number(val2);
+        break;
+    }
+    document.getElementById('calc').value = answer;
+    document.getElementById('op').value = '';
+    lastAnsw = answer;
+    val1 = answer;
+    val2 = '';
+    op = '';
+}
+
+function equals() {
+    calculate();
+    lastHist = history;
+    prevAns = lastAnsw;
+    history = lastHist + ' = ' + prevAns;
+    document.getElementById('fullHist').innerHTML = history;
+    clearCalc();
+}
+
+function clearNum() {
+    document.getElementById('calc').value = 0;
+}
+
+function makeNeg() {
+    const num = document.getElementById('calc').value;
+
+    if ( num === 0 || isNaN( num )) {
+        // allows a leading 0 with decimal to happen
+        document.getElementById('calc').value = 0;
+    } else if ( val2 === '' ) {
+        document.getElementById('calc').value = Number( num ) * -1;
+        val1 = document.getElementById('calc').value;
+    } else {
+        document.getElementById('calc').value = Number( num ) * -1;
+        val2 = document.getElementById('calc').value;
+    }
+}
+
+function allClearNum() {
+    // clears input to 0 and clears all variables
+    clearCalc();
+    trashHist();
+}
+
+function clearCalc() {
+    document.getElementById('calc').value = 0;
+    document.getElementById('op').value = '';
+    document.getElementById('hist').innerHTML = '';
+
+    val1 = '';
+    val2 = '';
+    op = '';
+    lastAnsw = undefined;
+    history = '';
+}
+
+function ans() {
+    document.getElementById('calc').value = prevAns;
+    val1 = prevAns;
+    val2 = '';
+    op = '';
+    history = '';
+    document.getElementById('hist').innerHTML = history;
+}
+
+function decimal() {
+
+}
+
+function trashHist() {
+    document.getElementById('fullHist').innerHTML = '';
+}
